558e71d3612333f7e2724934c6bbaa90c6db519d
Veregorn <rjbarco@gmail.com>
2024-03-12 11:56:12 +0100

    State variable for dinamic shop info

--- src/components/Stores.jsx
+++ src/components/Stores.jsx
@@ -1,23 +1,43 @@
 import '../styles/Stores.css'
 import { Link } from 'react-router-dom'
+import { useState } from 'react';
 
 function Stores() {
+
+    const [store, setStore] = useState('Boston');
+
+    const handleSelectedStore = (event) => {
+        setStore(event.target.value);
+    }
+
+    let source = '../../public/images/boston-store.webp';
+
+    if (store == 'Boston') {
+        source = '../../public/images/boston-store.webp';
+    } else if (store == 'Chicago') {
+        source = '../../public/images/chicago-store.jpg';
+    } else if (store == 'London') {
+        source = '../../public/images/london-store.jpg';
+    } else if (store == 'Los Angeles') {
+        source = '../../public/images/los-angeles-store.jpg';
+    } else if (store == 'New York City') {
+        source = '../../public/images/new-york-store.jpg';
+    }
+
     return (
         <div className="stores">
             <div className='left-side'>
                 <div className='store-vertical-selector'>
-                    <form>
-                        <select id="storeSelector" name="storeSelector">
-                            <option value="boston">Boston</option>
-                            <option value="chicago">Chicago</option>
-                            <option value="london">London</option>
-                            <option value="losAngeles">Los Angeles</option>
-                            <option value="newYorkCity">New York City</option>
-                        </select>
-                    </form>
+                    <select id="storeSelector" name="storeSelector" value={store} onChange={handleSelectedStore}>
+                        <option value="Boston">Boston</option>
+                        <option value="Chicago">Chicago</option>
+                        <option value="London">London</option>
+                        <option value="Los Angeles">Los Angeles</option>
+                        <option value="New York City">New York City</option>
+                    </select>
                 </div>
-                <h1>Find a store near you</h1>
-                <p>Our stores are located in the most important cities in the world. Visit us and check out our products in real life.</p>
+                {store == 'Boston' && <h1>Where all started</h1> || store == 'Chicago' && <h1>Our second home</h1> || store == 'London' && <h1>Our European square</h1> || store == 'Los Angeles' && <h1>Our sunny place</h1> || store == 'New York City' && <h1>Our big store</h1>}
+                {store == 'Boston' && <p>10 Bond St., Boston, BT 10012</p> || store == 'Chicago' && <p>10 Mc Yellow St., Chicago, CH 45498</p> || store == 'London' && <p>123 Curry Chicken St., London, LN 23621</p> || store == 'Los Angeles' && <p>234 Melrose Place Av., Los Angeles, LA 90210</p> || store == 'New York City' && <p>59 St., New York City, NY 33226</p>}
                 <div className='time-table'>
                     <h2>Opening hours</h2>
                     <p><strong>Monday to Friday:</strong> 10am - 8pm</p>
@@ -30,7 +50,7 @@ function Stores() {
                 </div>
             </div>
             <div className='right-side'>
-                <img className='stores-img' src="../../public/images/london-store.webp" alt="Stores" />
+                <img className='stores-img' src={source} alt="Stores" />
             </div>
         </div>
     );
